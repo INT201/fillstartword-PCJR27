b95b8c2cb4a897a64083d2e7cb01fb942d037ef3
porcha <porcha.por@mail.kmutt.ac.th>
2022-09-08 01:26:53 +0700

    Update firststep

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,13 +1,17 @@
  const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
+let words =word
+let stars = startWord
  if(word === null || word === undefined){
   return undefined
- }else if(word === startWord){
+ }else if(words.indexOf(stars)>=0){
   return word
  }else
  return startWord+word
  
 }
-
+console.log(fillStartWord('first', 'firststep'))
+console.log(fillStartWord('1-2565-', 'Hello world'))
+console.log(fillStartWord('first', null))
 module.exports = fillStartWord
